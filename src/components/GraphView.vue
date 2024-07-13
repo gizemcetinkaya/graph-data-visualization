a8d966dda6b4ac73f6eb5f3a99cdfefc77d6a637
Gizem Selli <cetinkayagizem@gmail.com>
2024-07-13 21:06:17 +0200

    Create main Vue components: App, GraphView, NodeDetails

---
+++ src/components/GraphView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="graph-view">
+        <h2>Graph View</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'GraphView',
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/NodeDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="node-details">
+        <h2>Node Details</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'NodeDetails',
+});
+</script>
+
+<style scoped></style>
